a24afce20d2bf25d3930f66c12c2259b7c9c731f
sadambekov <bekavlad2005@gmail.com>
2023-01-18 15:10:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let myName = "Bekmyrza";
+
+// console.log(myName);
+// console.log("My name's length is " + myName.length);
+// console.log(myName.toLowerCase());
+// console.log(myName.toUpperCase());
+
+let pi = 3.141593;
+
+// console.log(pi);
+// console.log(pi.toFixed(2));
+// console.log(pi.toFixed(0));
+
+let ex1 = 2 + 2;
+let ex2 = 2 - 2;
+let ex3 = 2 * 2;
+let ex4 = 2 / 2;
+
+let ex5 = 2 ** 4;
+let ex6 = 81 ** (1 / 3); //куб корень
+
+ex7 = 5 % 2;
+
+console.log(ex5);
+console.log(ex6);
+console.log(ex7);
+
+let ex8 = "hello " + "world";
+
+let ex9 = 4 + 10 + 12;
+let ex10 = 1 + 5 * 3;
+let ex11 = 15 * (1 - 1);
+
+let ex12 = 5 + "3";
+let ex13 = "5" + 3;
+let ex14 = "5" * 3;
+let ex15 = "a" * 3;
+
+console.log(ex12);
+console.log(ex13);
+console.log(ex14);
+console.log(ex15);
+
+let ex16 = -15;
+let ex17 = +"15";
