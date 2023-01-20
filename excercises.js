9933e958bc4ffa2bfadb2a103f000ef466df0398
sadambekov <bekavlad2005@gmail.com>
2023-01-20 10:09:56 +0600

    home work

---
+++ excercises.js
@@ -0,0 +1,37 @@
+let height = 5;
+let width = 8;
+let triangleArea = (height * width) / 2;
+let resultEx1 = "площадь треугольника равняется " + triangleArea + " см";
+console.log(resultEx1);
+
+let hours = 56;
+let minutes = hours * 60;
+let resultEx2 = "в " + hours + " часах " + minutes + " минут";
+console.log(resultEx2);
+
+let workDay = 8;
+let oneHours$ = 50;
+let oneWorkDayhours = 8;
+let end$ = oneHours$ * oneWorkDayhours * workDay;
+let resultEx3 = "программист заработал " + end$ + " $";
+console.log(resultEx3);
+
+let twoPoints = 8 * 2;
+let threePoints = 6 * 3;
+let endPoints = twoPoints + threePoints;
+let resultEx4 = "баскетбольная команда забила " + endPoints + " очков";
+console.log(resultEx4);
+
+let tractorFuelconsump = 22;
+let tractor22lKm = 100;
+let tractor100lKm = tractor22lKm * tractorFuelconsump;
+let resultEx5 =
+  "трактор проедет на " + tractor22lKm + " литрах " + tractor100lKm + "км";
+console.log(resultEx5);
+
+let cowFeet = 5 * 4;
+let chickenFeet = 6 * 2;
+let sheepfeet = 12 * 4;
+let leg = cowFeet + chickenFeet + sheepfeet;
+let resultEx6 = "кол-во ног на ферме " + leg;
+console.log(resultEx6);
